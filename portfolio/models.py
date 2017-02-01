71b6af6fcf27f561a22dc0029669d5b0070c251d
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-01 11:23:16 -0600

    Add Stock model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Stock(models.Model):
+
+    name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=25)
+
+    def __str__(self):
+
+        return self.symbol + ' : ' + self.name
+
