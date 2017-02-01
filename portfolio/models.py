f15ce8852dc203aec716b16705cbbfd809310552
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-01 14:34:40 -0600

    Update Stock model to create instance from json

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,8 +5,33 @@ from django.db import models
 
 class Stock(models.Model):
 
+    
     name = models.CharField(max_length=200)
     symbol = models.CharField(max_length=25)
+
+    @staticmethod
+    def from_json(in_json):
+        """
+        A static method to create an instance of stock with the inputs from json
+        retrieved using api. 
+
+        :param in_json (json): stock data in json format retrieved by api
+
+        :return (Stock): an instance of Stock class with attributes from json.
+        """
+
+        if 'symbol' not in in_json:
+            print("Stock Not Available")
+            return None
+
+        stock, created = Stock.objects.get_or_create(symbol=in_json['symbol'])
+
+        if created:
+
+            stock['name'] = in_json['name']
+            stock.save()
+
+        return stock 
 
     def __str__(self):
 
