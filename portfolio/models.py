4a818a6fbd6e2113ddad922af47d457eca7e7a24
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-02 14:50:53 -0600

    Add buy and sell methods

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 
 # Create your models here.
 
@@ -38,8 +38,94 @@ class Stock(models.Model):
         return self.symbol + ' : ' + self.name
 
 
-class StockQuery(models.Model):
+class Portfolio(models.Model):
 
+    username = models.CharField(max_length=20)
+    amount = models.DecimalField(min_value=0, 
+                                 max_digits=15, 
+                                 decimal_places=2, 
+                                 default_value=100000.00)
+
+    def buy_stock(self, query_stock, quantity):
+
+        stock = stock_query.stock
+        buy_price = stock_query.bid_price
+
+        # Check if the amount is enough
+
+        buy_amount = buy_price * quantity
+
+        if buy_amount > buy_price:
+
+            # throw an error here later.  
+            print("You don't have enough amount to make this transaction.")
+            return
+
+        try:
+            with transaction.atomic():
+                self.amount = self.amount - buy_amount
+                self.save() 
+
+                portfolio_entry = PortfolioEntry.objects.filter(
+                                            username=request.username,
+                                            stock=stock,
+                                            buy_price=price)
+
+                if portfolio_entry:
+                    portfolio_entry.quantity = portfolio_entry.quantity + quantity
+                    portfolio_entry.save()
+                else:
+                    PortfolioEntry.objects.create( 
+                                           username=request.username,
+                                           stock=stock,
+                                           buy_price=price,
+                                           quantity=quantity)
+            
+        except IntegrityError:
+            pass
+
+
+    def sell_stock(self, query_stock, quantity):
+
+        stock = stock_query.stock
+        sell_price = stock_query.bid_price
+
+        portfolio_entries = PortfolioEntry.objects.filter(
+                                         username=request.username,
+                                         stock=stock).order_by('-buy_price')
+        available_quantity = 0
+
+        if portfolio_entries:  
+            available_quantity = sum(portfolio_entries, key=lambda entry:entry.quantity)
+
+        if available_quantity < quantity:
+         
+            # throw an error here later.
+            print("You don't have enough shares to make this transaction.")
+            return
+
+        try:
+            with transaction.atomic():
+
+                remaining_quantity = quantity
+
+                for entry in portfolio_entries:
+                    if entry.quantity < remaining_quantity:
+                        remaining_quantity = remaining_quantity - entry.quantity
+                        entry.delete()
+                    else:
+                        entry.quantity = entry.quantity - remaining_quantity
+                        entry.save()
+                        break
+
+                self.amount = self.amount + sell_price*quantity
+
+        except IntegrityError:
+            pass
+
+
+class StockQuery(models.Model):
+ 
     name = models.CharField(max_length=200)
     symbol = models.CharField(max_length=25)
     bid_price = models.DecimalField(max_digits=15, decimal_places=2)
@@ -47,7 +133,7 @@ class StockQuery(models.Model):
 
 
 class PortfolioEntry(models.Model):
-
-    name = models.CharField(max_length=200)
-    symbol = models.CharField(max_length=25)
+    user_name = models.CharField(max_length=20) 
+    stock = models.ForeignKey('portfolio.Stock')
     buy_price = models.DecimalField(max_digits=15, decimal_places=2)
+    quantity = models.IntegerField(min_value=1)
