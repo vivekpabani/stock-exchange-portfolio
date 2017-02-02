a8447b396b9bd589e4eae91b606c842914f27350
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-02 17:43:06 -0600

    Update home view to verify form get

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .models import Stock, StockQuery, Portfolio, PortfolioEntry
+
+
+def home(request):
+
+    username = request.session.get('username', '')
+
+    if not username:
+        return redirect('authenticate/login')
+    request.username = username
+    context = dict()
+
+    if 'symbol' in request.GET:
+
+        print(request.GET['symbol']) 
+
+
+    return render(request, 'portfolio/home.html', context=context)
+
+
+
+def buy_shares(request):
+
+    pass
+
+
+def sell_shares(request):
+
+    pass
