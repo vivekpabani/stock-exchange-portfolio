ad671502a178002b0c161affeaa62c4190213bb2
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-04 15:57:44 -0600

    Remove extra models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Stock, Portfolio, PortfolioEntry, OrderHistory 
+
+admin.site.register(Stock)
+admin.site.register(Portfolio)
+admin.site.register(PortfolioEntry)
+admin.site.register(OrderHistory)
