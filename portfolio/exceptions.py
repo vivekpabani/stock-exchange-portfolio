be890c74e1f4a19a61b7263d9ccab7bd3d42e5e4
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-04 15:59:23 -0600

    Add exception classes

---
+++ portfolio/exceptions.py
@@ -0,0 +1,15 @@
+
+class StockDoesNotExistException(Exception):
+    pass
+
+class NotEnoughAmountForTransactionException(Exception):
+    pass
+
+class NotEnoughSharesForTransactionException(Exception):
+    pass
+
+class CannotConnectToServerException(Exception):
+    pass
+
+class InternalTransactionException(Exception):
+    pass
