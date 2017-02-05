1c226153c404e1af0891d85c9458eeaf8ee60287
Vivek Pabani <vpabani@hawk.iit.edu>
2017-02-04 23:51:08 -0600

    Update wsgi for heroku

--- stockexchange/wsgi.py
+++ stockexchange/wsgi.py
@@ -1,16 +1,18 @@
 """
-WSGI config for stockexchange project.
+WSGI config for mywebpage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stockexchange.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
